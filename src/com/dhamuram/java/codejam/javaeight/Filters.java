c84911d64255522e3cf85fe22927eb6bad721e89
dhamu-ram <dhamu.sciera@gmail.com>
2021-10-17 18:52:55 +0530

    Filter Sample

--- src/com/dhamuram/java/codejam/javaeight/Filters.java
+++ src/com/dhamuram/java/codejam/javaeight/Filters.java
@@ -1,11 +1,17 @@
 package com.dhamuram.java.codejam.javaeight;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Filters {
     public static void main(String[] args) {
-        Integer[] intArray = {10, 12, 13, 20, 35};
-        
-        Arrays.stream(intArray).filter(int i -> (i % 2)
+
+        List<Integer> num = Arrays.asList(1,2,3,4,5);
+
+        List<Integer> collect = num.stream().map(n -> n * 2).collect(Collectors.toList());
+
+        System.out.println(collect); //[2, 4, 6, 8, 10]
+
     }
 }
