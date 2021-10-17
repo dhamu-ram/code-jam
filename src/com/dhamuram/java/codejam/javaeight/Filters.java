149067b07b6ba1e4ebb3239b52005ac30671755e
dhamu-ram <dhamu.sciera@gmail.com>
2021-10-17 18:36:57 +0530

    Filter Sample

---
+++ src/com/dhamuram/java/codejam/javaeight/Filters.java
@@ -0,0 +1,11 @@
+package com.dhamuram.java.codejam.javaeight;
+
+import java.util.Arrays;
+
+public class Filters {
+    public static void main(String[] args) {
+        Integer[] intArray = {10, 12, 13, 20, 35};
+        
+        Arrays.stream(intArray).filter(int i -> (i % 2)
+    }
+}
