61af19ee1b1e870fa7ddec4a018f68f367b15a5f
dhamu-ram <dhamu.sciera@gmail.com>
2021-11-24 23:02:10 +0530

    JavaCollection

---
+++ src/com/dhamuram/java/codejam/javaeight/JavaCollection.java
@@ -0,0 +1,16 @@
+package com.dhamuram.java.codejam.javaeight;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class JavaCollection {
+    public static void main(String[] args) {
+
+        List<String> strings = Arrays.asList("drgs", "crgs", "brgs", "trgs", "ergs");
+
+        Collections.sort(strings);
+
+        System.out.println(strings);
+    }
+}
