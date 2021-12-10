8656c5d00ba98fd27a7cd5cf72bde73cd8d4a766
dhamu-ram <dhamu.sciera@gmail.com>
2021-12-10 22:16:01 +0530

    FunctionalInterfaceSample

---
+++ src/com/dhamuram/java/codejam/java/eight/FunctionalInterfaceSample.java
@@ -0,0 +1,18 @@
+package com.dhamuram.java.codejam.java.eight;
+
+@FunctionalInterface
+interface FuncInterface {
+
+    int multiply(int a, int b);
+}
+
+public class FunctionalInterfaceSample {
+
+    public static void main(String args[]) {
+
+        FuncInterface Total = (a, b) ->  a * b;
+
+        // simple operation of multiplication of 'a' and 'b'
+        System.out.println("Result: " + Total.multiply(30, 60));
+    }
+}
