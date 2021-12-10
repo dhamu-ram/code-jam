e6a7c72225e7e3fa36f1096cc7e7378f818b07b3
dhamu-ram <dhamu.sciera@gmail.com>
2021-12-10 22:25:21 +0530

    Compare

---
+++ src/com/dhamuram/java/codejam/java/eight/Compare.java
@@ -0,0 +1,15 @@
+package com.dhamuram.java.codejam.java.eight;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Compare {
+    public static void main(String[] args) {
+        List<Integer> integers = Arrays.asList(12, 34, 11,45,88, 34, 1, 10, 99);
+
+        Collections.sort(integers, (i, j) -> i.compareTo(j));
+
+        System.out.println( integers.toString());
+    }
+}
