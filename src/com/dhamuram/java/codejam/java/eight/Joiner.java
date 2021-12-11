73d050cbc915c7dc89461139b7b430e0b721c3f1
dhamu-ram <dhamu.sciera@gmail.com>
2021-12-11 06:59:37 +0530

    Joiner

---
+++ src/com/dhamuram/java/codejam/java/eight/Joiner.java
@@ -0,0 +1,14 @@
+package com.dhamuram.java.codejam.java.eight;
+
+import java.util.StringJoiner;
+
+public class Joiner {
+    public static void main(String[] args) {
+        StringJoiner joiner = new StringJoiner("/");
+        joiner.add("user");
+        joiner.add("data");
+        joiner.add("codeJam");
+
+        System.out.println(joiner.toString());
+    }
+}
