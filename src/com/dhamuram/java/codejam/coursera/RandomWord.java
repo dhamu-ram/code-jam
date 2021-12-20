7f5bb80e68b23f0b3c6fb0891122b0a158093f02
dhamu-ram <dhamu.sciera@gmail.com>
2021-12-21 00:00:00 +0530

    Concurrent HashMap

---
+++ src/com/dhamuram/java/codejam/coursera/RandomWord.java
@@ -0,0 +1,14 @@
+package com.dhamuram.java.codejam.coursera;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        StdIn.readString(System.in);
+        StdIn.isEmpty();
+        StdOut.println();
+        StdRandom.bernoulli();
+    }
+}
