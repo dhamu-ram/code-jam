12cfe60bea2cef365d9df4b67b7e2db407c66663
Dhamu <dhamu.sciera@gmail.com>
2022-01-02 18:27:43 +0530

    ValidParentheses

--- src/com/dhamuram/java/codejam/macode/ValidParentheses.java
+++ src/com/dhamuram/java/codejam/macode/ValidParentheses.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class ValidParentheses {
     public static void main(String[] args) {
         ValidParentheses ValidParentheses = new ValidParentheses();
-        String s = "]";
+        String s = "{)){}";
         boolean isValid = ValidParentheses.isValid(s);
         System.out.println(isValid);
     }
@@ -13,22 +13,18 @@ public class ValidParentheses {
     private boolean isValid(String s) {
 
         Stack<Character> stack = new Stack();
-        stack.push(s.charAt(0));
 
-        for (int i = 0; i < s.length(); i++) {
-            Character c = s.charAt(i);
-            if (stack.size() != 0 && stack.peek() == c) {
-                stack.pop();
-            } else {
-                if (c == '{') {
-                    stack.push('}');
-                } else if (c == '[') {
-                    stack.push(']');
-                } else if (c == '(') {
-                    stack.push(')');
-                }
+        for (char c : s.toCharArray()) {
+            if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '(') {
+                stack.push(')');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
             }
         }
-        return stack.size() == 0;
+        return stack.isEmpty();
     }
 }
