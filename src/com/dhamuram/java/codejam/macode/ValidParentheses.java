412dc409e631894c7d5c38b7d1713bb287e2e3e1
Dhamu <dhamu.sciera@gmail.com>
2022-01-02 09:08:00 +0530

    ValidParentheses

--- src/com/dhamuram/java/codejam/macode/ValidParentheses.java
+++ src/com/dhamuram/java/codejam/macode/ValidParentheses.java
@@ -1,16 +1,34 @@
 package com.dhamuram.java.codejam.macode;
 
+import java.util.Stack;
+
 public class ValidParentheses {
     public static void main(String[] args) {
         ValidParentheses ValidParentheses = new ValidParentheses();
-        String s = "{}";
+        String s = "]";
         boolean isValid = ValidParentheses.isValid(s);
+        System.out.println(isValid);
     }
 
     private boolean isValid(String s) {
 
-        
+        Stack<Character> stack = new Stack();
+        stack.push(s.charAt(0));
 
-        return  true;
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            if (stack.size() != 0 && stack.peek() == c) {
+                stack.pop();
+            } else {
+                if (c == '{') {
+                    stack.push('}');
+                } else if (c == '[') {
+                    stack.push(']');
+                } else if (c == '(') {
+                    stack.push(')');
+                }
+            }
+        }
+        return stack.size() == 0;
     }
 }
