0704a09cc65dec9d9b810d02f923b1bc68b590dc
dhamu-ram <ra.dhamodharan@gmail.com>
2026-05-08 20:00:21 -0700

    code change

--- src/main/com/dhamuram/codejam/leetcode/NeedleHaystick.java
+++ src/main/com/dhamuram/codejam/leetcode/NeedleHaystick.java
@@ -9,20 +9,19 @@ public class NeedleHaystick {
         System.out.println("Needle  : " + needle);
         System.out.println("Index   : " + index);
     }
-}
 
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) return 0;
 
-public int strStr(String haystack, String needle) {
-    if (needle.isEmpty()) return 0;
+        int h = haystack.length();
+        int n = needle.length();
 
-    int h = haystack.length();
-    int n = needle.length();
-
-    for (int i = 0; i <= h - n; i++) {
-        if (haystack.substring(i, i + n).equals(needle)) {
-            return i;
+        for (int i = 0; i <= h - n; i++) {
+            if (haystack.substring(i, i + n).equals(needle)) {
+                return i;
+            }
         }
-    }
 
-    return -1;
+        return -1;
+    }
 }
