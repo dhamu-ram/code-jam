95f59aaeb4be07b831ef0d2f208ef747b084ee59
dhamu-ram <ra.dhamodharan@gmail.com>
2026-05-08 19:53:31 -0700

    Needle and Haystick

---
+++ src/main/com/dhamuram/codejam/leetcode/NeedleHaystick.java
@@ -0,0 +1,28 @@
+public class NeedleHaystick {
+    public static void main(String[] args) {
+        String haystack = "hello";
+        String needle = "ll";
+
+        int index = new NeedleHaystick().strStr(haystack, needle);
+
+        System.out.println("Haystack: " + haystack);
+        System.out.println("Needle  : " + needle);
+        System.out.println("Index   : " + index);
+    }
+}
+
+
+public int strStr(String haystack, String needle) {
+    if (needle.isEmpty()) return 0;
+
+    int h = haystack.length();
+    int n = needle.length();
+
+    for (int i = 0; i <= h - n; i++) {
+        if (haystack.substring(i, i + n).equals(needle)) {
+            return i;
+        }
+    }
+
+    return -1;
+}
